bcad824495cdebfba8a4a8aeada02b0711412f6f
Tor Kjetil Nilsen <tor@arkitektum.no>
2019-09-02 16:46:41 +0200

    Add project files.

---
+++ ks.fiks.io.fagsystem.arkiv.sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ks.fiks.io.fagsystem.arkiv.sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
