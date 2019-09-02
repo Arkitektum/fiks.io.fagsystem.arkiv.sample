a7ec01773f4fe07810300282638518efa567ddce
Tor Kjetil Nilsen <tor@arkitektum.no>
2019-09-02 16:47:38 +0200

    init

--- ks.fiks.io.fagsystem.arkiv.sample/Program.cs
+++ ks.fiks.io.fagsystem.arkiv.sample/Program.cs
@@ -6,7 +6,7 @@ namespace ks.fiks.io.fagsystem.arkiv.sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
